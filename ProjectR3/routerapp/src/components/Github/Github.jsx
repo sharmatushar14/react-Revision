b62aae903d290b01db0d2efa1dc95401b5de0cb9
sharmatushar14 <sharmatusharofficial14@gmail.com>
2024-05-13 12:13:12 +0530

    Added the router and loader functionality for optimization

---
+++ ProjectR3/routerapp/src/components/Github/Github.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+const Github = () => {
+    const data = useLoaderData();
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      Github Followers: {data.followers}
+      <img src={data.avatar_url} alt='Github Picture' width={300}/>
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async()=>{
+    const response = await fetch('https://api.github.com/users/sharmatushar14')
+    return response.json();
+}
--- ProjectR3/routerapp/src/index.js
+++ ProjectR3/routerapp/src/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Layout from './Layout';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+import Github from './components/Github/Github';
+import { githubInfoLoader } from './components/Github/Github';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route
+      loader={githubInfoLoader}
+      path='github' element={<Github/>}/>
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
